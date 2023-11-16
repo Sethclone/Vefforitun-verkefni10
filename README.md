ffd7bcff306d38d083079342e0f61e291128adc2
Unnar <unnareh@gmail.com>
2023-11-16 15:29:48 +0000

    Second iteration, added styling buttons etc.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Vefforitun-verkefni10
+
+Þetta verkefni notar Vite, Vue og Bootstrap.
+
+Allt nema bootstrap er algjörlega nýtt fyrir mér þannig ég gerði mitt besta með að reyna vinna með nýju frameworki.
+
+Ég notaði npm install vite. og svo create-vite fids-website til að búa til projectið og valdi vue sem frameworkið og svo bootstrap sem css. Svo setti eg bootstrap og AXIOS í import. Til að læra á hvernig vue virkar fylgi ég tutorial á youtube og bjó til HelloWorld.vue 
+
+Svo fór ég að vinna á FlightsList.vue sem er aðalefni síðunnar. FIDS er notað á flugvellinum og stendur fyrir Flight information display system. API sem ég fæ kemur frá ISAVIA og er beintengdur við upplýsingarnar sem hægt að fá á flugvellinum. Síðan er hentug þeim sem vinna á flugvellinum þar sem FIDS síðan er ekki vel optimized til að sýna aðeins notanlegar upplýsingar sem tengjast vinnu. Þetta er verkefni byggt af prototýpu sem ég gerði fyrir ári meðan ég starfaði á flugvellinum.
+
+Síðan er mjög einföld, Hún sýnir flug sem koma til íslands og eru svo meðhöndluð af annarhvort APA(Airport Associates) eða IGS(Icelandair Ground Services) Það þæginlega við þetta er að APA tekur yfirleitt við Economy flugum meðan IGS tekur við þeim á dýra kantinum. Þannig þegar filterað er fyrir flight_handler þá koma upplýsingar fyrir economy flugum og Icelandair flugum(og samstarfsfyrirtæki þeirra)
--- fids-website/src/components/FlightsList.vue
+++ fids-website/src/components/FlightsList.vue
@@ -1,81 +1,89 @@
 <!-- FlightsList.vue -->
 <template>
-    <div class="bg-dark">
-        <h1 class="text-light">Incoming Flights to Keflavík</h1>
-      <table class="table table-dark table-striped table-hover">
-        <thead>
-          <tr>
-            <th scope="col">Flight</th>
-            <th scope="col">Origin</th>
-            <th scope="col">Status</th>
-            <th scope="col">Scheduled Time</th>
-            <th scope="col">Airline</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="flight in filteredFlights" :key="flight.flt">
-            <td scope="row">{{ flight.flt }}</td>
-            <td>{{ flight.origin }}</td>
-            <td>{{ flight.status }}</td>
-            <td>{{ flight.sched_time.slice(11, 16) }}</td>
-            <td>{{ flight.airline_name }}</td>
-          </tr>
-        </tbody>
-      </table>
+  <div class="bg-dark">
+    <h1 class="text-light">Incoming Flights to Keflavík</h1>
+
+    <div class="mb-3">
+      <button @click="selectHandler('APA')" :class="{ 'btn-primary': selectedHandler === 'APA' }" class="btn btn-secondary">Economy Flights</button>
+      <button @click="selectHandler('IGS')" :class="{ 'btn-primary': selectedHandler === 'IGS' }" class="btn btn-secondary">Icelandair Flights</button>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        flights: [],
-      };
+
+    <table class="table table-dark table-striped table-hover">
+      <thead>
+        <tr>
+          <th scope="col">Flight</th>
+          <th scope="col">Origin</th>
+          <th scope="col">Status</th>
+          <th scope="col">Scheduled Time</th>
+          <th scope="col">Airline</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="flight in filteredFlights" :key="flight.flt">
+          <td scope="row">{{ flight.flt }}</td>
+          <td>{{ flight.origin }}</td>
+          <td>{{ flight.status }}</td>
+          <td>{{ flight.sched_time.slice(11, 16) }}</td>
+          <td>{{ flight.airline_name }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      flights: [],
+      selectedHandler: 'APA', // Default selected handling agent
+    };
+  },
+  computed: {
+    filteredFlights() {
+      return this.flights.filter(
+        (flight) =>
+          (this.selectedHandler === 'APA' && flight.handling_agent === 'APA') ||
+          (this.selectedHandler === 'IGS' && flight.handling_agent === 'IGS') &&
+          flight.flight_type === 'J' &&
+          flight.destination === 'Keflavík'
+      );
     },
-    computed: {
-      filteredFlights() {
-        // Filter the flights based on your conditions
-        return this.flights.filter(
-          (flight) =>
-            flight.handling_agent === 'APA' &&
-            flight.flight_type === 'J' &&
-            flight.destination === 'Keflavík'
-        );
-      },
+  },
+  methods: {
+    getToday(offset) {
+      const today = new Date();
+      today.setDate(today.getDate() + offset);
+      const isoDate = today.toISOString().split('T')[0];
+      return isoDate;
     },
-    methods: {
-      getToday(offset) {
-        const today = new Date();
-        today.setDate(today.getDate() + offset);
-        const isoDate = today.toISOString().split('T')[0];
-        return isoDate;
-      },
-      async fetchFlights() {
-        try {
-          const response = await this.$axios.get(
-            'http://fids.kefairport.is/api/flights',
-            {
-              params: {
-                dateFrom: this.getToday(0),
-                dateTo: this.getToday(1),
-              },
-            }
-          );
-  
-          console.log(response.data); // Log the API response
-          this.flights = response.data;
-        } catch (error) {
-          console.error('Error fetching flights:', error);
-        }
-      },
+    async fetchFlights() {
+      try {
+        const response = await this.$axios.get('http://fids.kefairport.is/api/flights', {
+          params: {
+            dateFrom: this.getToday(0),
+            dateTo: this.getToday(1),
+            handlingAgent: this.selectedHandler,
+          },
+        });
+
+        console.log(response.data); // Log the API response
+        this.flights = response.data;
+      } catch (error) {
+        console.error('Error fetching flights:', error);
+      }
     },
-    mounted() {
+    selectHandler(handler) {
+      this.selectedHandler = handler;
       this.fetchFlights();
     },
-  };
-  </script>
-  
-  <style>
-  /* Your component styles here */
-  </style>
-  
+  },
+  mounted() {
+    this.fetchFlights();
+  },
+};
+</script>
+
+<style>
+/* Your component styles here */
+</style>
